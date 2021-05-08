19f78b813692af39e9173a880d0538302bdbd3c1
Andrés <andressanchezro@hotmail.com>
2021-05-09 00:12:28 +0200

    feat(front): add pets table

--- src/frontend/src/pages/home/home.tsx
+++ src/frontend/src/pages/home/home.tsx
@@ -1,4 +1,10 @@
+import {
+	DetailsList,
+	DetailsListLayoutMode,
+	IColumn,
+} from '@fluentui/react/lib/components/DetailsList';
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { petApi } from '../../api';
 import { ODataResponse, Pet } from '../../models';
 
@@ -13,11 +19,67 @@ const Home: React.FC = () => {
 	useEffect(() => {
 		getPets();
 	}, []);
+
+	const _columns = [
+		{
+			key: 'Name',
+			name: 'Name',
+			fieldName: 'name',
+			minWidth: 100,
+			maxWidth: 200,
+			isResizable: true,
+		},
+		{
+			key: 'Age',
+			name: 'Age',
+			fieldName: 'age',
+			minWidth: 100,
+			maxWidth: 200,
+			isResizable: true,
+		},
+		{
+			key: 'Species',
+			name: 'Species',
+			fieldName: 'species',
+			minWidth: 100,
+			isResizable: true,
+		},
+		{
+			key: 'NavBtn',
+			name: 'Detail',
+			fieldName: 'id',
+			minWidth: 100,
+		},
+	];
+	const renderColumn = (
+		item?: any,
+		index?: number | undefined,
+		column?: IColumn | undefined,
+	): React.ReactNode => {
+		if (column?.key === 'Age') {
+			return `${item.age} years old`;
+		} else if (column?.key === 'Name') {
+			return `${item.name}`;
+		} else if (column?.key === 'Species') {
+			return `${item.species}`;
+		} else if (column?.key === 'NavBtn') {
+			return (
+				<NavLink to={`/pet/${item.id}`} activeClassName="selected">
+					Detail
+				</NavLink>
+			);
+		}
+	};
 	return (
 		<>
-			{pets.map((pet: Pet) => {
-				return <p>{pet.name?.toLocaleLowerCase()}</p>;
-			})}
+			<h1>Pets</h1>
+			<DetailsList
+				compact={true}
+				items={pets}
+				columns={_columns}
+				layoutMode={DetailsListLayoutMode.justified}
+				onRenderItemColumn={renderColumn}
+			/>
 		</>
 	);
 };
